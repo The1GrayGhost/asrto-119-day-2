b423fa857166aa935121e9e04418dfa02e1d13fc
The1GrayGhost <43651619+The1GrayGhost@users.noreply.github.com>
2018-10-02 11:27:04 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,38 @@
+x=9
+y=3
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y) #modulus
+print(x**y) #exponentiation
+x=9.191823
+print(x//y) #floor division 
+
+#assignment operators
+x=9 #set x equal to 9
+x+=3 #sets x equal to x+3
+print(x)
+x=9
+x-=3
+print(x)
+x=9
+x*=3
+print(x)
+x=9
+x/=3
+print(x)
+x=9
+x**=3
+print(x)
+
+#comparison operators
+x=9
+y=3
+print(x==y) #True if x=y, false otherwise
+print(x!=y) #false if x=y, true otherwise
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
